c6151a959b5d94b2cac619a8d405df22dce53a9e
Dawid Dzitko <dawid.dzitko@teb.edu.pl>
2023-09-13 12:39:54 +0200

    koniec

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,14 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
             button1.BackColor = Color.Sienna;
             button1.ForeColor = SystemColors.ButtonHighlight;
-            button1.Location = new Point(357, 190);
+            button1.Location = new Point(647, 352);
             button1.Name = "button1";
             button1.Size = new Size(141, 86);
             button1.TabIndex = 0;
@@ -43,19 +48,48 @@
             button1.UseVisualStyleBackColor = false;
             button1.Click += buttonFirst;
             // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick_1;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(59, 17);
+            toolStripStatusLabel1.Text = "punkty : 0";
+            toolStripStatusLabel1.Click += toolStripStatusLabel1_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -2,9 +2,15 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        readonly Random r;
+        int punkty;
+
         public Form1()
         {
             InitializeComponent();
+            r = new Random();
+            punkty = 0;
+            timer1.Start();
         }
 
         private void buttonFirst(object sender, EventArgs e)
@@ -21,7 +27,7 @@ namespace WinFormsApp1
             widowHeight -= button1.Height;
 
             //instalacjia generatora randomowych Liczb
-            Random rnd = new Random();  
+            Random rnd = new Random();
 
             //losowa odleglosc od lewej - nie wiecej niz szerokosc okna 
             int randomLeft = rnd.Next(windowWidth);
@@ -35,6 +41,39 @@ namespace WinFormsApp1
             button1.Location = TopleftCorner;
 
 
+            int maxX = this.Size.Width - button1.Size.Width - 30;
+            int maxY = this.Size.Height - button1.Size.Height - 30;
+            Point p = new Point();
+            p.X = r.Next(12, maxX);
+            p.Y = r.Next(12, maxY);
+            button1.Location = p;
+            punkty++;
+            toolStripStatusLabel1.Text = "Punkty : " + punkty.ToString();
+            timer1.Stop();
+            timer1.Start();
+
+
+
         }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            
+            int maxX = this.Size.Width - button1.Size.Width - 30;
+            int maxY = this.Size.Height - button1.Size.Height - 30;
+            Point p = new Point();
+            p.X = r.Next(12, maxX);
+            p.Y = r.Next(12, maxY);
+            button1.Location = p;
+            punkty = 0;
+            toolStripStatusLabel1.Text = "Punkty : " + punkty.ToString();
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
